9712052655896045c076b02f2dd15174987e33e4
Alexander Kazakov <alexander@kazakov.mobi>
2019-04-09 14:43:01 -0700

    Adds table header for search results, indicating the number of matches over the total number of records.

--- Day33/Project7/PetitionApp/PetitionApp/ViewController.swift
+++ Day33/Project7/PetitionApp/PetitionApp/ViewController.swift
@@ -9,19 +9,33 @@
 import UIKit
 
 class ViewController: UITableViewController {
-
+  
   var petitions = [Petition]()
-  private var API_URL: String!
+  var filteredPetitions = [Petition]()
+  var searchController = UISearchController()
+  var searchFooter: SearchFooter!
+  var API_URL: String!
+  var isFiltering: Bool {
+    return !isSearchFieldEmpty() && searchController.isActive
+  }
   
   override func viewDidLoad() {
     super.viewDidLoad()
+   
+    searchController                = setupSearchController()
+    searchFooter                    = SearchFooter(frame: CGRect(origin: .zero, size: CGSize(width: view.frame.width, height: 50)))
+    navigationItem.searchController = searchController
+    tableView.tableFooterView       = searchFooter
     
-    if navigationController?.tabBarItem.tag == 0 {
-      // urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+    // By setting definesPresentationContext on your view controller to true, you ensure that the search bar does not remain on the screen if the user navigates to another view controller while the UISearchController is active.
+    definesPresentationContext = true
+    
+    if (navigationController?.tabBarItem.tag) == 0 {
+      // https://api.whitehouse.gov/v1/petitions.json?limit=100
       API_URL = "https://www.hackingwithswift.com/samples/petitions-1.json"
       title = "MOST RECENT"
     } else {
-      // urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
+      // https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100
       API_URL = "https://www.hackingwithswift.com/samples/petitions-2.json"
       title = "TOP RATED"
     }
@@ -35,7 +49,15 @@ class ViewController: UITableViewController {
     navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Credits", style: .plain, target: self, action: #selector(showCredits))
     
   }
-
+  
+  func setupSearchController() -> UISearchController {
+    let controller = UISearchController(searchResultsController: nil)
+    controller.searchResultsUpdater = self
+    controller.obscuresBackgroundDuringPresentation = false
+    controller.searchBar.placeholder = "Search Petitions"
+    return controller
+  }
+  
   @objc func showCredits() {
     let vc = UIAlertController(title: "CREDITS", message: "This application uses the public API of the White House, located at: https://petitions.whitehouse.gov/developers", preferredStyle: .alert)
     vc.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
@@ -63,13 +85,17 @@ class ViewController: UITableViewController {
   }
   
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return petitions.count
+    if isFiltering {
+      searchFooter.setIsFilteringToShow(filteredItemCount: filteredPetitions.count, of: petitions.count)
+      return filteredPetitions.count
+    } else {
+      return petitions.count
+    }
   }
   
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-    
-    let petition = petitions[indexPath.row]
+    let petition = (isFiltering) ? filteredPetitions[indexPath.row] : petitions[indexPath.row]
     
     cell.textLabel?.text = petition.title
     cell.detailTextLabel?.text = petition.body
@@ -82,5 +108,23 @@ class ViewController: UITableViewController {
     navigationController?.pushViewController(vc, animated: true)
   }
   
+  func isSearchFieldEmpty() -> Bool {
+    return searchController.searchBar.text?.isEmpty ?? true
+  }
+  
 }
 
+extension ViewController: UISearchResultsUpdating {
+  
+  func updateSearchResults(for searchController: UISearchController) {
+    filteredPetitions.removeAll(keepingCapacity: false)
+    guard let searchQuery = searchController.searchBar.text?.lowercased() else { return }
+    
+    filteredPetitions = petitions.filter { (petition) -> Bool in
+      return (petition.title.lowercased().contains(searchQuery) || petition.body.lowercased().contains(searchQuery))
+    }
+    
+    tableView.reloadData()
+  }
+  
+}
