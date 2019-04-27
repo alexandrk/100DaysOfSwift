1f8567052aa7c3074fad391bd1fbc637ddf8dff0
Alexander Kazakov <alexander@kazakov.mobi>
2019-04-26 20:18:13 -0700

    Project 1: Adds views counter to each photo with persistance through UserDefaults.

--- Day 16-18/Project 1/StormViewer/ViewController.swift
+++ Day 16-18/Project 1/StormViewer/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UITableViewController {
 
   var pictures = [String]()
+  var counter = [String: Int]()
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -18,6 +19,13 @@ class ViewController: UITableViewController {
     title = "Storm Viewer"
     navigationController?.navigationBar.prefersLargeTitles = true
     
+    // Add Recommend button to nav bar
+    navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Recommend to others", style: .plain, target: self, action: #selector(recommendAppPopover))
+    
+    loadData()
+  }
+
+  func loadData() {
     DispatchQueue.global(qos: .background).async { [weak self] in
       let fm = FileManager.default
       let path = Bundle.main.resourceURL!
@@ -30,15 +38,17 @@ class ViewController: UITableViewController {
         }
       }
       
+      // Load view counter from UserDefaults
+      if let savedCounter = UserDefaults.standard.object(forKey: "counter") as? [String: Int] {
+        self?.counter = savedCounter
+      }
+      
       DispatchQueue.main.async { [weak self] in
         self?.tableView.reloadData()
       }
     }
-    
-    // Add Recommend button to nav bar
-    navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Recommend to others", style: .plain, target: self, action: #selector(recommendAppPopover))
   }
-
+  
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return pictures.count
   }
@@ -49,6 +59,7 @@ class ViewController: UITableViewController {
     cell.imageView?.image = image(UIImage(named: pictures[indexPath.row], in: Bundle.main, compatibleWith: nil)!, withSize: CGSize(width: 50, height: 50))
     
     cell.textLabel?.text = pictures[indexPath.row]
+    cell.detailTextLabel?.text = "Number of views: \(counter[pictures[indexPath.row]] ?? 0)"
     
     return cell
   }
@@ -58,6 +69,12 @@ class ViewController: UITableViewController {
     if let detailViewController = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
       detailViewController.selectedImage = pictures[indexPath.row]
       detailViewController.navBarTitle = "Picture \(indexPath.row + 1) of \(pictures.count)"
+      
+      // Add to count of views for a given picture
+      counter[pictures[indexPath.row]] = (counter[pictures[indexPath.row]] ?? 0) + 1
+      tableView.reloadRows(at: [indexPath], with: .none)
+      save()
+      
       navigationController?.pushViewController(detailViewController, animated: true)
     }
     
@@ -86,6 +103,11 @@ class ViewController: UITableViewController {
     let vc = UIActivityViewController(activityItems: ["Check out this cool app that I'm using: '\(applicationName!)'."], applicationActivities: nil)
     vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
     present(vc, animated: true, completion: nil)
+  }
+  
+  func save() {
+    let userDefaults = UserDefaults.standard
+    userDefaults.set(counter, forKey: "counter")
   }
   
 }
