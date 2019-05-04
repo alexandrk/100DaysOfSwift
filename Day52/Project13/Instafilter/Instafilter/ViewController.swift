439144b237717f8004a1ed62540846cfef004746
Alexander Kazakov <alexander@kazakov.mobi>
2019-05-03 22:36:13 -0700

    Project 13: Changes the app flow, allowing for each filter to be controlled by their appropriate sliders for more fine grain filter application .

--- Day52/Project13/Instafilter/Instafilter/ViewController.swift
+++ Day52/Project13/Instafilter/Instafilter/ViewController.swift
@@ -12,8 +12,26 @@ import UIKit
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
   @IBOutlet weak var imageView: UIImageView!
-  @IBOutlet weak var intensity: UISlider!
+  @IBOutlet weak var intensitySlider: UISlider!
+  @IBOutlet weak var radiusSlider: UISlider!
+  @IBOutlet weak var centerXSlider: UISlider!
+  @IBOutlet weak var centerYSlider: UISlider!
+  @IBOutlet weak var scaleSlider: UISlider!
+  @IBOutlet weak var angleSlider: UISlider!
+  
+  @IBOutlet weak var intensitySliderView: UIStackView!
+  @IBOutlet weak var radiusSliderView: UIStackView!
+  @IBOutlet weak var centerSliderView: UIStackView!
+  
+  @IBOutlet weak var centerXMaxLabel: UILabel!
+  @IBOutlet weak var centerYMaxLabel: UILabel!
+  
+  @IBOutlet weak var angleSliderView: UIStackView!
+  @IBOutlet weak var scaleSliderView: UIStackView!
+  
   @IBOutlet weak var changeFilterButton: UIButton!
+  @IBOutlet weak var saveButton: UIButton!
+  
   var currentImage: UIImage!
   
   var context: CIContext!
@@ -25,10 +43,20 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     title = "Instafilter"
     navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
     
+    if imageView.image == nil {
+      intensitySliderView.isHidden = true
+      radiusSliderView.isHidden = true
+      centerSliderView.isHidden = true
+      angleSliderView.isHidden = true
+      scaleSliderView.isHidden = true
+    }
+    
     context = CIContext()
-    currentFilter = CIFilter(name: "CISepiaTone")
   }
   
+  /***
+   UIBarButtonItem handler for load picture
+   */
   @objc func importPicture(_ sender: UIBarButtonItem) {
     
     let ac = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
@@ -46,6 +74,9 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     present(ac, animated: true)
   }
 
+  /***
+   Call Image picker from UIBarButtonItem handler
+   */
   func showPicker(_ action: UIAlertAction) {
     guard let actionTitle = action.title else { return }
     
@@ -58,6 +89,9 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     present(picker, animated: true)
   }
   
+  /***
+   ImagePicker handler
+  */
   func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
     var image: UIImage!
     
@@ -73,12 +107,44 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     dismiss(animated: true)
     
     currentImage = image
+    imageView.image = currentImage
     
-    let beginImage = CIImage(image: currentImage)
-    currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
-    applyProcessing()
+    resetSlidersSelectedValues()
+    setSlidersValuesBasedOnImageDimensions(image)
   }
   
+  /***
+   Sets sliders values to 0 when new image is loaded or new filter is selected.
+   */
+  func resetSlidersSelectedValues() {
+    intensitySlider.value = 0
+    radiusSlider.value = 0
+    centerXSlider.value = 0
+    centerYSlider.value = 0
+    scaleSlider.value = 0
+    angleSlider.value = 0
+  }
+  
+  /***
+   Sets max values for sliders based on image dimensions in imageView
+   */
+  fileprivate func setSlidersValuesBasedOnImageDimensions(_ image: UIImage) {
+    let xScale = image.size.width / imageView.bounds.width
+    let yScale = image.size.height / imageView.bounds.height
+    
+    let coeff = max(xScale, yScale)
+    
+    centerXSlider.maximumValue = Float(image.size.width / coeff)
+    centerYSlider.maximumValue = Float(image.size.height / coeff)
+    centerXMaxLabel.text = "\(Int(centerXSlider.maximumValue))"
+    centerYMaxLabel.text = "\(Int(centerYSlider.maximumValue))"
+    
+    radiusSlider.maximumValue = centerXSlider.maximumValue
+  }
+  
+  /***
+   Change Filter button press handler.
+  */
   @IBAction func changeFilter(_ sender: UIButton) {
     let ac = UIAlertController(title: "Choose Filter", message: nil, preferredStyle: .actionSheet)
     
@@ -99,10 +165,15 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     present(ac, animated: true)
   }
   
+  /***
+   Filter Selection Handler
+  */
   func setFilter(action: UIAlertAction) {
-    guard currentImage != nil else { showMessage(title: "No Image Selected!", message: "Please select an image first."); return }
-    guard let actionTitle = action.title else { return }
+    resetSlidersSelectedValues()
     
+    if currentImage == nil { showMessage(title: "No Image Selected!", message: "Please select an image first."); return }
+    
+    guard let actionTitle = action.title else { return }
     currentFilter = CIFilter(name: actionTitle)
     
     changeFilterButton.setTitle(actionTitle, for: .normal)
@@ -113,7 +184,63 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+    
+    setActiveSliders()
+    
     applyProcessing()
+  }
+  
+  func setActiveSliders() {
+    let filterKeys = currentFilter.inputKeys.compactMap { $0.lowercased() }.joined(separator: ",")
+    
+    // Intensity
+    intensitySliderView.isHidden = !filterKeys.contains("intensity")
+    intensitySlider.isEnabled = !intensitySliderView.isHidden
+    
+    //Radius
+    radiusSliderView.isHidden = !filterKeys.contains("radius")
+    radiusSlider.isEnabled = !radiusSliderView.isHidden
+    
+    // Center
+    centerSliderView.isHidden = !filterKeys.contains("center")
+    centerXSlider.isEnabled = !centerSliderView.isHidden
+    centerYSlider.isEnabled = !centerSliderView.isHidden
+    
+    //Scale
+    scaleSliderView.isHidden = !filterKeys.contains("scale")
+    scaleSlider.isEnabled = !scaleSliderView.isHidden
+    
+    // Angle
+    angleSliderView.isHidden = !filterKeys.contains("angle")
+    angleSlider.isEnabled = !angleSliderView.isHidden
+    
+  }
+  
+  func applyProcessing(slider: UISlider? = nil) {
+    let inputKeys = currentFilter.inputKeys
+    
+    if inputKeys.contains(kCIInputIntensityKey) {
+      currentFilter.setValue(intensitySlider.value, forKey: kCIInputIntensityKey)
+    }
+    if inputKeys.contains(kCIInputRadiusKey) {
+      currentFilter.setValue(radiusSlider.value, forKey: kCIInputRadiusKey)
+    }
+    if inputKeys.contains(kCIInputScaleKey) {
+      currentFilter.setValue(scaleSlider.value, forKey: kCIInputScaleKey)
+    }
+    if inputKeys.contains(kCIInputCenterKey) {
+      currentFilter.setValue(CIVector(x: CGFloat(centerXSlider!.value * 2), y: CGFloat(centerYSlider!.value * 2)), forKey: kCIInputCenterKey)
+    }
+    if inputKeys.contains(kCIInputAngleKey) {
+      currentFilter.setValue(NSNumber(value: angleSlider.value), forKey: kCIInputAngleKey)
+    }
+    
+    guard let outputImage = currentFilter.outputImage else { return }
+    
+    if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+      let processedImage = UIImage(cgImage: cgImage)
+      imageView.image = processedImage
+    }
   }
   
   @IBAction func save(_ sender: Any) {
@@ -131,24 +258,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     present(vc, animated: true)
   }
   
-  @IBAction func intensityChanged(_ sender: Any) {
-    applyProcessing()
-  }
-  
-  func applyProcessing() {
-    let inputKeys = currentFilter.inputKeys
-    
-    if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(intensity.value, forKey: kCIInputIntensityKey) }
-    if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(intensity.value * 200, forKey: kCIInputRadiusKey) }
-    if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(intensity.value * 10, forKey: kCIInputScaleKey) }
-    if inputKeys.contains(kCIInputCenterKey) { currentFilter.setValue(CIVector(x: currentImage.size.width / 2, y: currentImage.size.height / 2), forKey: kCIInputCenterKey) }
-    
-    guard let outputImage = currentFilter.outputImage else { return }
-    
-    if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
-      let processedImage = UIImage(cgImage: cgImage)
-      imageView.image = processedImage
-    }
+  @IBAction func sliderChanged(_ sender: UISlider) {
+    applyProcessing(slider: sender)
   }
   
   @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
