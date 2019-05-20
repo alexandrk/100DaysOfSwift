e9699a9549b34b62bd9b872bbe5f08be812dce51
Alexander Kazakov <alexander@kazakov.mobi>
2019-05-19 21:32:54 -0700

    Project17: Initial commit with 'Play again' implemented.

---
+++ Day62/Project17/README.md
@@ -0,0 +1,7 @@
+Simple space shooter game.
+
+Implements:
+  - Per-pixel collision detection
+  - Advancing particle systems
+  - Timer
+  - Liner and angular damping 
---
+++ Day62/Project17/SpaceAdvanture/SpaceAdvanture/GameScene.swift
@@ -0,0 +1,159 @@
+//
+//  GameScene.swift
+//  SpaceAdvanture
+//
+//  Created by Alexander Kazakov on 5/18/19.
+//  Copyright © 2019 Alexander Kazakov. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
+  
+  var starfield: SKEmitterNode!
+  var player: SKSpriteNode!
+  
+  var scoreLabel: SKLabelNode!
+  var playAgain: SKLabelNode!
+  
+  var possibleEmenies = ["ball", "hammer", "tv"]
+  var gameTimer: Timer?
+  var isGameOver = false
+  
+  var score = 0 {
+    didSet {
+      scoreLabel.text = "Score: \(score)"
+    }
+  }
+  
+  override func didMove(to view: SKView) {
+    backgroundColor = .black
+    
+    starfield = SKEmitterNode(fileNamed: "starfield")!
+    starfield.position = CGPoint(x: 1024, y: 384)
+    starfield.advanceSimulationTime(10)
+    addChild(starfield)
+    starfield.zPosition = -1
+    
+    createPlayer()
+    
+    scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+    scoreLabel.position = CGPoint(x: 16, y: 16)
+    scoreLabel.horizontalAlignmentMode = .left
+    addChild(scoreLabel)
+    
+    score = 0
+    
+    physicsWorld.gravity = .zero
+    physicsWorld.contactDelegate = self
+    
+    gameTimer = Timer.scheduledTimer(timeInterval: 0.35, target: self, selector: #selector(createEnemy), userInfo: nil, repeats: true)
+  }
+  
+  func createPlayer() {
+    player = SKSpriteNode(imageNamed: "player")
+    player.position = CGPoint(x: 100, y: 384)
+    player.physicsBody = SKPhysicsBody(texture: player.texture!, size: player.size)
+    player.physicsBody?.allowsRotation = false
+    player.physicsBody?.contactTestBitMask = 1
+    addChild(player)
+  }
+  
+  @objc func createEnemy() {
+    guard let enemy = possibleEmenies.randomElement() else { return }
+    
+    let sprite = SKSpriteNode(imageNamed: enemy)
+    sprite.position = CGPoint(x: 1200, y: Int.random(in: 50...736))
+    sprite.name = "enemy"
+    addChild(sprite)
+    
+    sprite.physicsBody = SKPhysicsBody(texture: sprite.texture!, size: sprite.size)
+    sprite.physicsBody?.categoryBitMask = 1
+    sprite.physicsBody?.velocity = CGVector(dx: -500, dy: 0)
+    sprite.physicsBody?.angularVelocity = 5
+    sprite.physicsBody?.linearDamping = 0
+    sprite.physicsBody?.angularDamping = 0
+  }
+  
+  override func update(_ currentTime: TimeInterval) {
+    for node in children {
+      if node.position.x < -300 {
+        node.removeFromParent()
+      }
+    }
+    
+    if !isGameOver {
+      score += 1
+    }
+  }
+  
+  override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    guard let touch = touches.first else { return }
+    var location = touch.location(in: self)
+    if location.y < 100 {
+      location.y = 100
+    } else if location.y > 668 {
+      location.y = 668
+    }
+    
+    player.position = location
+  }
+  
+  override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    guard let touch = touches.first else { return }
+    let location = touch.location(in: self)
+    let nodesAtLocation = nodes(at: location)
+    
+    if nodesAtLocation.contains(where: { $0.name == "playAgain" }) {
+      restartGame()
+    }
+  }
+  
+  func didBegin(_ contact: SKPhysicsContact) {
+    let explosion = SKEmitterNode(fileNamed: "explosion")!
+    explosion.position = player.position
+    addChild(explosion)
+    
+    player.removeFromParent()
+    isGameOver = true
+    
+    showPlayAgainLabel()
+  }
+  
+  func showPlayAgainLabel() {
+    
+    if playAgain == nil {
+      playAgain = SKLabelNode(fontNamed: "Chalkduster")
+      
+      playAgain.text = "Play Again?"
+      playAgain.name = "playAgain"
+      playAgain.fontSize = 30
+      playAgain.position = CGPoint(x: 512, y: 383)
+      playAgain.horizontalAlignmentMode = .center
+      playAgain.zPosition = 3
+      playAgain.xScale = 0.001
+      playAgain.yScale = 0.001
+      addChild(playAgain)
+
+      let playAgainAction = SKAction.scale(to: 1.0, duration: 0.5)
+      playAgain.run(playAgainAction)
+    }
+    
+  }
+  
+  @objc func restartGame() {
+    for node in children {
+      if node.name == "enemy" {
+        node.removeFromParent()
+      }
+    }
+    score = 0
+    createPlayer()
+    isGameOver = false
+    
+    playAgain.removeFromParent()
+    playAgain = nil
+    
+  }
+}
