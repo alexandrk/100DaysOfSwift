f9d060a9fbb61ec676ff58b287a0ff677549141c
Alexander Kazakov <alexander@kazakov.mobi>
2019-06-03 21:30:59 -0700

    Project19: Adds a textfield for writing custom javascript to be sent back and executed on the Safari's webpage that the extension was called from.

--- Day67/Project19/Safari extension/Extension/Action.js
+++ Day67/Project19/Safari extension/Extension/Action.js
@@ -3,12 +3,15 @@ var Action = function() {}
 
 Action.prototype = {
   
+  // Used to pass parameters / info to the extension code (ActionViewController:viewDidLoad() method)
   run: function(parameters) {
     parameters.completionFunction({"URL": document.URL, "title": document.title});
   },
   
+  // Callback, called when the extension is finished execution
   finalize: function(parameters) {
-    
+    var customJavascript = parameters["customJavascript"];
+    eval(customJavascript);
   }
   
 };
--- Day67/Project19/Safari extension/Extension/ActionViewController.swift
+++ Day67/Project19/Safari extension/Extension/ActionViewController.swift
@@ -11,29 +11,44 @@ import MobileCoreServices
 
 class ActionViewController: UIViewController {
 
-    @IBOutlet weak var imageView: UIImageView!
-
-    override func viewDidLoad() {
-      super.viewDidLoad()
-    
-      if let inputItem = extensionContext?.inputItems.first as? NSExtensionItem {
-        if let itemProvider = inputItem.attachments?.first {
-          itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) { [weak self] (dict, error) in
-            // do stuff
-            guard let itemDictionary = dict as? NSDictionary else { return }
-            guard let javascriptValues = itemDictionary[NSExtensionJavaScriptPreprocessingResultsKey] as? NSDictionary else { return }
-            
-            print(javascriptValues)
+  @IBOutlet weak var script: UITextView!
+  var pageTitle = ""
+  var pageURL = ""
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  
+    navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+  
+    // Parse the parameters that were passed from the webpage through the extension to the here
+    if let inputItem = extensionContext?.inputItems.first as? NSExtensionItem {
+      if let itemProvider = inputItem.attachments?.first {
+        itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) { [weak self] (dict, error) in
+          
+          guard let itemDictionary = dict as? NSDictionary else { return }
+          guard let javascriptValues = itemDictionary[NSExtensionJavaScriptPreprocessingResultsKey] as? NSDictionary else { return }
+          
+          self?.pageTitle = javascriptValues["title"] as? String ?? ""
+          self?.pageURL = javascriptValues["URL"] as? String ?? ""
+          
+          DispatchQueue.main.async {
+            self?.title = self?.pageTitle
           }
         }
       }
+    }
       
-    }
+  }
 
-    @IBAction func done() {
-        // Return any edited content to the host app.
-        // This template doesn't do anything, so we just echo the passed in items.
-        self.extensionContext!.completeRequest(returningItems: self.extensionContext!.inputItems, completionHandler: nil)
-    }
+  /// Event Handler for the navigation bar button item
+  @IBAction func done() {
+    // Create the return parameters to be sent back to Safari's page through the extension
+    let item = NSExtensionItem()
+    let argument: NSDictionary = ["customJavascript": script.text ?? ""]
+    let webDictionary: NSDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: argument]
+    let customJavascript = NSItemProvider(item: webDictionary, typeIdentifier: kUTTypePropertyList as String)
+    item.attachments = [customJavascript]
+    extensionContext?.completeRequest(returningItems: [item])
+  }
 
 }
