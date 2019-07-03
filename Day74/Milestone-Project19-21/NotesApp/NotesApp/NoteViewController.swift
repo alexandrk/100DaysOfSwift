4a92939e5749f2a5123f151d0ab21993d3c97ccb
Alexander Kazakov <alexander@kazakov.mobi>
2019-07-03 16:35:25 -0700

    Project 19-21 (Milestone): Adds keyboard handling for UITextView.

--- Day74/Milestone-Project19-21/NotesApp/NotesApp/NoteViewController.swift
+++ Day74/Milestone-Project19-21/NotesApp/NotesApp/NoteViewController.swift
@@ -22,6 +22,11 @@ class NoteViewController: UIViewController {
     existingNoteID = note?.id
     textView.text = note?.content
     
+    // Keyboard Adjustments
+    let notificationCenter = NotificationCenter.default
+    notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+    notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+    
     // Navigation Bar Items
     navigationItem.setRightBarButtonItems([
       UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(saveNote))
@@ -65,6 +70,24 @@ class NoteViewController: UIViewController {
     
     // Go back to list of notes
     navigationController?.popViewController(animated: true)
+  }
+  
+  @objc func adjustForKeyboard(notification: Notification) {
+    guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+    
+    let keyboardScreenEndFrame = keyboardValue.cgRectValue
+    let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+    
+    if notification.name == UIResponder.keyboardWillHideNotification {
+      textView.contentInset = .zero
+    } else {
+      textView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
+    }
+    
+    textView.scrollIndicatorInsets = textView.contentInset
+    
+    let selectedRange = textView.selectedRange
+    textView.scrollRangeToVisible(selectedRange)
   }
   
 }
