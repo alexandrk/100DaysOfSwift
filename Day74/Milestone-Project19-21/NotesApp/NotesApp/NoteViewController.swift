766cdea91a932049de9440161bc447be8a9a36c9
Alexander Kazakov <alexander@kazakov.mobi>
2019-07-05 20:57:37 -0700

    Project 19-21 (Milestone): Adds toolbar items for deleting and creating new note.

--- Day74/Milestone-Project19-21/NotesApp/NotesApp/NoteViewController.swift
+++ Day74/Milestone-Project19-21/NotesApp/NotesApp/NoteViewController.swift
@@ -14,6 +14,7 @@ class NoteViewController: UIViewController {
   private let filename = "notes.json"
   var note: Note?
   var existingNoteID: String?
+  var notes = [Note]()
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -22,6 +23,8 @@ class NoteViewController: UIViewController {
     existingNoteID = note?.id
     textView.text = note?.content
     
+    populateNotesArray()
+    
     // Keyboard Adjustments
     let notificationCenter = NotificationCenter.default
     notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
@@ -29,13 +32,65 @@ class NoteViewController: UIViewController {
     
     // Navigation Bar Items
     navigationItem.setRightBarButtonItems([
-      UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(saveNote))
+      UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(saveNoteAction))
     ], animated: true)
+    
+    // Toolbar Items
+    navigationController?.isToolbarHidden = false
+    setToolbarItems([
+      UIBarButtonItem(title: "Delete", style: .done, target: self, action: #selector(deleteNoteAction)),
+      UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil),
+      UIBarButtonItem(barButtonSystemItem: .compose, target: self, action: #selector(createNewNoteAction))
+      ], animated: true)
+    
   }
 
-  @objc func saveNote() {
-    var notes = [Note]()
-    
+  @objc func saveNoteAction() {
+    saveNote()
+    // Go back to list of notes
+    navigationController?.popViewController(animated: true)
+  
+  }
+  
+  @objc func deleteNoteAction() {
+    deleteExistingNote()
+    navigationController?.popViewController(animated: true)
+  }
+  
+  @objc func createNewNoteAction() {
+    saveNote()
+    note = nil
+    existingNoteID = nil
+    textView.text = ""
+  }
+  
+  private func deleteExistingNote() {
+    if let existingNoteID = existingNoteID {
+      removeNoteFromNotes(noteID: existingNoteID)
+      Storage.store(notes, to: .documents, as: filename)
+    }
+  }
+  
+  fileprivate func populateNotesArray() {
+    if notes.isEmpty {
+      if Storage.fileExists(filename, in: .documents) {
+        do {
+          notes = try Storage.retrieve(filename, from: .documents, as: [Note].self)
+        } catch {
+          print(error)
+          // Remove file, if unable to read from it
+          Storage.remove(filename, from: .documents)
+        }
+      }
+    }
+  }
+  
+  fileprivate func removeNoteFromNotes(noteID: String) {
+    let existingNoteIndex = notes.firstIndex(where: { $0.id == noteID })
+    if existingNoteIndex != nil { notes.remove(at: existingNoteIndex!) }
+  }
+  
+  func saveNote() {
     // Check if there is any data in the note, RETURN if empty.
     if textView.text.isEmpty { return }
     
@@ -46,30 +101,14 @@ class NoteViewController: UIViewController {
       note = Note(content: textView.text)
     }
     
-    // If other notes exist, retrieve them from file
-    if Storage.fileExists(filename, in: .documents) {
-      do {
-        notes = try Storage.retrieve(filename, from: .documents, as: [Note].self)
-        // If editing an existing Note find it's ID within the list to modify it istead of creating a new one
-        if let existingNoteID = existingNoteID {
-          let existingNoteIndex = notes.firstIndex(where: { $0.id == existingNoteID })
-          if existingNoteIndex != nil { notes.remove(at: existingNoteIndex!) }
-        }
-      } catch {
-        print(error)
-        // Remove file, if unable to read from it
-        Storage.remove(filename, from: .documents)
-      }
-    }
+    // If editing an existing Note find it's ID within the list to modify it istead of creating a new one
+    deleteExistingNote()
     
     // Prepend the new note to the beginning of the list
     notes.insert(note!, at: 0)
     
     // Save to file
     Storage.store(notes, to: .documents, as: filename)
-    
-    // Go back to list of notes
-    navigationController?.popViewController(animated: true)
   }
   
   @objc func adjustForKeyboard(notification: Notification) {
